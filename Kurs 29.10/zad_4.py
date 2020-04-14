2b24799aa1d137ff8874b20f4e742fd2c5b59b5f
szymon <szymonsaletnik@wp.pl>
2020-04-14 19:23:38 +0200

    wszystko

--- Kurs 29.10/zad_4.py
+++ Kurs 29.10/zad_4.py
@@ -5,4 +5,19 @@ cena = 10.0
 waga = 2.5
 naleznosc = cena * waga
 
+# Cena za kg: 10.0
+# Waga: 2.5
+# Należność: 25.0
 
+print("Cena za kg: " + str(cena) + " zl")
+print("Cena za kg:", cena, "zl")
+print("Waga:", waga, "kg")
+print("Naleznosc:", naleznosc, "zl")
+
+# f-string - to takie ustrojstwo, gdzie przy tworzeniu napisu
+# dajemy na poczatku f a pozniej wewnatrz tego napisu mozemy sie odwolywac do zmiennych
+# https://realpython.com/python-f-strings/
+
+print(f"Cena za kg: {cena} zl")
+print(f"Waga: {waga} kg")
+print(f"Naleznosc: {naleznosc} zl")
--- Kurs 29.10/zad_7.py
+++ Kurs 29.10/zad_7.py
@@ -1,6 +1,6 @@
 # Napisz program sprawdzający czy podana przez użytkownika liczba:
 
-# - jest nieparzysta, podzielna przez 3 i większa od 10
+# - jest podzielna przez 2, podzielna przez 3 i większa od 10
 # - lub jest to liczba 7.
 
 liczba = int(input("podaj liczbe: "))
