ddddc28a1fe28efdd2a273c89fc49baca2213d08
szymon <szymonsaletnik@wp.pl>
2020-05-09 10:13:33 +0200

    Odp 2.

--- Kurs 19.04/zad_0006.py
+++ Kurs 19.04/zad_0006.py
@@ -5,17 +5,18 @@
 #>>> silnia(5)
 #120
 
-x = input('Podaj liczbe: ')
+def silnia(n):
+    if n < 0:
+        return "Erron"
+    elif n == 0:
+        return 1
+    else:
+        return n * silnia(n-1)
 
-silnia = 1
-
-if x > 0:
-    for i in range(1, 1 + x):
-        silnia *= i
-    print
-    'Podana liczba to: %3f' % x
-    print
-    'Silnia dla podanej liczby wynosi: %10f' % silnia
-else:
-    print
-    'Podana liczba jest mniejsza od zera'
+def test_silnia():
+    assert silnia(0) == 1
+    assert silnia(1) == 1
+    assert silnia(2) == 2
+    assert silnia(3) == 6
+    assert silnia(5) == 120
+    assert silnia(-5) == "Error"
