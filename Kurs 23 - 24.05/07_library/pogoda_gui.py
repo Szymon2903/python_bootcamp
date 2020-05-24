c1deb9beb90613dcb10a6f8f9a137ba482bbb976
szymon <szymonsaletnik@wp.pl>
2020-05-24 15:32:23 +0200

    Kurs 23-24.zadania

--- Kurs 23 - 24.05/07_library/pogoda_gui.py
+++ Kurs 23 - 24.05/07_library/pogoda_gui.py
@@ -1,20 +1,26 @@
-import crime
-from tkinter import massagebox
+import tkinter
+
 from pogoda import get_location_id, get_location_weather, report
 
 def przygotuj_raport():
-    a_entry.get()
+    location_name = a_entry.get()
+    location_id = get_location_id(location_name)
+    weather = get_location_wearther(location_id)
+    rep = report(weather, location_name)
+    wynik.configure(text=rep)
 
 root = tkinter.TK()
-a_label = tkinter.Label(master=root, text="Wypisz miasto")
+a_label = tkinter.Label(master=root, text="Miasto")
 a_label.pack()
 a_entry = tkinter.Entry(master=root)
 a_entry.pack()
-wynik_labl = tkinter.Label(master=root, text="Dane pogody: ")
+wynik_labl = tkinter.Label(master=root, text="Dane o pogodzie: ")
 wynik = tkinter.Label(master=root, text="")
 wynik_labl.pack()
 wynik.pack()
-submit = tkinter.Button(master=root, text=f"Szukaj", command=report)
+
+submit = tkinter.Button(master=root, text="Znajdź", command=get_report)
+
 submit.pack()
 root.mainloop()
 
